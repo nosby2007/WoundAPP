2e8c428ec463a5659740a562043f8aa03acdcd0d
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-09-20 19:30:19 -0400

    fix(master): add isolated Firebase DEV environment to mobile app

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC5y4binHjnTkdYdYDbgNWPPhlfJk54bhY",
+    authDomain: "jade-dev-5b0e3.firebaseapp.com",
+    projectId: "jade-dev-5b0e3",
+    storageBucket: "jade-dev-5b0e3.firebasestorage.app",
+    messagingSenderId: "732091500513",
+    appId: "1:732091500513:web:62b08d7f657452ef5734a0"
+  }
+};
